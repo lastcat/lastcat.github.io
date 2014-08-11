d81ea14c0560f1d838a4c3cf58553c9d6a322ba2
lastcat <kokodoko966@gmail.com>
2014-08-12 06:01:02 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  //$("li").css("color","red");
+  //$("#siteTitle").css("color","red");
+  //$("h1 .pageTitle").css("color","red");
+  $(".articleDetailHead .pageTitle").css("color","red");
+});
